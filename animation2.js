24b340f0f51eb731315af8d732574dcc5048c487
Mlodziak00 <90759492+Mlodziak00@users.noreply.github.com>
2024-12-06 00:21:21 +0100

    Add files via upload

---
+++ animation2.js
@@ -0,0 +1,81 @@
+/*
+function changeImageOnHover(){
+    if(document.getElementsByClassName("Selectible")){
+        document.getElementsByClassName("Selectible").style.filter = "grayscale(0.25%)";
+        document.getElementsByClassName("Selectible").style.filter = "contrast(0.25%)";
+    }
+}
+changeImageOnHover();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+document.getElementById("missiontext1").innerHTML = "<div id='mission'>Our Mission</div><br>Our mission here at the Alterum Reality Testing Company is to bring a sense of understanding and <a href='assets/img/KeyCardTwo.png'>comprehension</a> about the world we live in, to people like you. We were founded in 1957, with the belief and strive to unlock the potential of our world and that beyond. We here at A.R.T. Co. are proud to say that we are constantly bringing forth innovation and insight like no other has. Albert Einstein once said “If at first the idea is not absurd, then there is no hope for it” and we have taken that to heart through our ruthless devotion to science.";
+document.getElementById("missiontext2").innerHTML = "<div id='mission'>Our Mission</div><br>Our mission here at the Alterum Reality Testing Company is to bring a sense of understanding and <a href='assets/img/KeyCardTwo.png'>comprehension</a> about the world we live in, to people like you. We were founded in 1957, with the belief and strive to unlock the potential of our world and that beyond. We here at A.R.T. Co. are proud to say that we are constantly bringing forth innovation and insight like no other has. Albert Einstein once said “If at first the idea is not absurd, then there is no hope for it” and we have taken that to heart through our ruthless devotion to science.";
+document.getElementById("doll1").innerHTML = "Dollie Parks joined Alterum Reality Testing Co. Or A.R.T. in modern year 2010 after many successful years at <a href='assets/zip/Key Card Two.zip'>Î´ßø¬å†´ ®ˆ∂©´ íç˙øø¬ øƒ â´∂ˆçˆ˜´</a>, as well as spending years following providing rather grand discoveries and movements in the world of medicine and patient care. Doctor Parks was brought on site to assist in the further discoveries and movements in the medical research and care A.R.T. provided, and soon worked to rise in the ranks of each team they were assigned to. Dollie Parks was officially given the title of Head of The Medical Division, as well as lead researcher in the S.S.M.D. Project located in Sector W in 2019, after being recruited by Eyre K. Bones.</td>";
+document.getElementById("doll2").innerHTML = "Dollie Parks joined Alterum Reality Testing Co. Or A.R.T. in modern year 2010 after many successful years at <a href='assets/zip/Key Card Two.zip'>Î´ßø¬å†´ ®ˆ∂©´ íç˙øø¬ øƒ â´∂ˆçˆ˜´</a>, as well as spending years following providing rather grand discoveries and movements in the world of medicine and patient care. Doctor Parks was brought on site to assist in the further discoveries and movements in the medical research and care A.R.T. provided, and soon worked to rise in the ranks of each team they were assigned to. Dollie Parks was officially given the title of Head of The Medical Division, as well as lead researcher in the S.S.M.D. Project located in Sector W in 2019, after being recruited by Eyre K. Bones.</td>";
+document.getElementById("dolli1").innerHTML = "<a href='assets/zip/Key Card Two (Expanded Ver. 2).zip'><img src='assets/img/Dollie.png' alt='Dollie' class='portraitimg'></a>";
+document.getElementById("dolli2").innerHTML = "<a href='assets/zip/Key Card Two (Expanded Ver. 2).zip'><img src='assets/img/Dollie.png' alt='Dollie' class='portraitimg'></a>";
+document.getElementById("blanki1").innerHTML = "<a href='assets/zip/Key Card Two (Expanded Ver. 1).zip'><img src='assets/img/Blank.png' alt='blank' class='portraitimg'></a>";
+document.getElementById("blanki2").innerHTML = "<a href='assets/zip/Key Card Two (Expanded Ver. 1).zip'><img src='assets/img/Blank.png' alt='blank' class='portraitimg'></a>";
